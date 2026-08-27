0085c406039980a218afb78b0bea5341e2b15d40
miguel ceballos <miguel.ceballosra@campusucc.edu.co>
2026-08-27 14:11:29 -0500

    ejercicios

---
+++ src/basic/c5_structures/Structuresexercises.java
@@ -0,0 +1,11 @@
+package basic.c5_structures;
+public class Structuresexercises {
+    public static void main(String[] args) {
+
+        // Crear un Array con 5 valores e imprimir la longitud
+
+        String  [] Arrays = new String [5];
+        System.out.println(Arrays[1]);
+        System.out.println("Holi");
+    }
+}
